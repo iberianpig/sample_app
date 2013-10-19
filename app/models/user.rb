ba9741b21e9a6cdc0a78262fe9db876c8a78515b
iberianpig <yhkyky@gmail.com>
2013-10-20 00:50:50 +0900

    6章終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  before_save{ self.email = email.downcase}
+  # before_save{ self.email = email.downcase}
+  before_save { email.downcase! }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # 大文字・小文字を無視した一意性の検証
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -88,5 +88,15 @@ describe User do
       before {@user.password = @user.password_confirmation = "a" * 5}
       it {should be_invalid}
     end
+
+    describe "email address with mixed cases" do
+      let(:mixed_case_email) { "HogeHoge@example.com" }
+
+      it "should be saved as all lower-case" do
+        @user.email=mixed_case_email
+        @user.save
+        expect(@user.reload.email).to eq mixed_case_email.downcase
+      end
+    end
   end
 end
