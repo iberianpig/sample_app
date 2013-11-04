a1740eb3961f073817d3bb8ba6e0cf01099e2090
iberianpig <yhkyky@gmail.com>
2013-11-04 16:14:33 +0900

    Finish sign in 8章終了

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,7 +10,7 @@ describe "User pages" do
     it {should have_title(user.name)}
   end
 
-describe "signup" do
+  describe "signup" do
 
     before { visit signup_path }
 
@@ -19,6 +19,13 @@ describe "signup" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before {click_button submit}
+
+        it{should have_title('Sign up')}
+        it{should have_content('error')}
       end
     end
 
@@ -32,6 +39,15 @@ describe "signup" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before {click_button submit}
+        let(:user){User.find_by(email: 'user@example.com')}
+
+        it{should have_link('Sign out')}
+        it{should have_title(user.name)}
+        it{should have_selector('div.alert.alert-success', text: 'Welcome')}
       end
     end
   end
